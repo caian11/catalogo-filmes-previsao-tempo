a64c4cd865f1d87155271f3e3bd4941e62a3a255
CaianH <caianhenrique11@gmail.com>
2025-11-26 20:34:18 -0400

    Commit inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace catalogo_filmes_previsao_tempo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
